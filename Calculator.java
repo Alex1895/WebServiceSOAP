2abf57c75bd15188df268627938bd7cf82fcb0f5
Alejandro_Alaniz <miguelitojacobito@gmail.com>
2018-10-03 09:14:06 -0500

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -1,5 +1,22 @@
 package com.hcl.soap.service;
-
+import javax.jws.WebService;
+import javax.jws.WebMethod;
+@WebService
 public class Calculator {
-
+  @WebMethod
+  public int sum(int n1, int n2){
+    return n1+n2;
+  }
+  @WebMethod
+  public int res(int n1, int n2){
+    return n1-n2;
+  }
+  @WebMethod
+  public int mul(int n1, int n2){
+    return n1*n2;
+  }
+  @WebMethod
+  public int div(int n1, int n2){
+    return n1/n2;
+  }
 }
