168daf4a25b9ae6f45740518736589b286d9f84d
Alejandro_Alaniz <miguelitojacobito@gmail.com>
2018-10-03 09:18:19 -0500

    Update Calculator_App.java

--- Calculator_App.java
+++ Calculator_App.java
@@ -4,7 +4,8 @@ public class Calculator_App {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		String url = "http://localhost:8039/Calculator";
+		Endpoint.publish("http://localhost:8039/Calculator", new Calculator());
+		//System.out.println();
 	}
-
 }
