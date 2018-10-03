c1a5eff3a91a0e28320f7a642e86fe68e3d7aa6a
Alejandro_Alaniz <miguelitojacobito@gmail.com>
2018-10-03 09:20:13 -0500

    Update Calculator_App.java

--- Calculator_App.java
+++ Calculator_App.java
@@ -1,7 +1,9 @@
 package com.hcl.soap.service;
-
+import javax.xml.ws.Endpoint;
+import javax.jws.WebService;
+@WebService
 public class Calculator_App {
-
+@WebEndpoint
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		String url = "http://localhost:8039/Calculator";
